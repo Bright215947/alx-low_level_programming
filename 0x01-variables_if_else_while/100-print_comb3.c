8ff69277550e03fa6b634c00e724f8b5c83f3310
Bright215947 <afeezakinrogba227@gmail.com>
2022-09-09 12:36:15 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers from 00 to 99
+  * main - Prints the numbers from 01 to 89
   *
   * Return: Always (Success)
   */
@@ -9,16 +9,16 @@ int main(void)
 {
 	int c, i;
 
-	c = i = '0';
+	c = i = '1';
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = '1'; c <= '8'; c++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		for (i = '1'; i <= '8'; i++)
 		{
 			putchar(c);
 			putchar(i);
 
-			if ((c != '9') || (c == '9' && i != '9'))
+			if ((c != '8') || (c == '8' && i != '8'))
 			{
 				putchar(',');
 				putchar(' ');
