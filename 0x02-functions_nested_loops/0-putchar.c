2dd125626a5142160e2d999403d49a82c8d0b496
Bright215947 <afeezakinrogba227@gmail.com>
2022-09-13 06:16:55 -0700

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- print _putchar
+ *
+ * description: following the process
+ */
+int main(void) 
+{char bless[50] =  (_putchar') {
+printf("%s\n" , bless);	}
+return 0;
+}	
