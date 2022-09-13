863d494fcf7732ee82660a8ec38f809545a78479
Bright215947 <afeezakinrogba227@gmail.com>
2022-09-13 08:35:26 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,6 +2,7 @@
 
 /**
   * main - Entry point
+  *
   * Return: Always 0
   */
 int main(void)
@@ -15,4 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0); }	
+
+	return (0); }
