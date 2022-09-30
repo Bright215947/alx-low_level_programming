07f81832206e5df9439692397eb1b3d172e851d5
Bright215947 <afeezakinrogba227@gmail.com>
2022-09-30 04:56:45 -0700

    task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,17 @@
 #include "main.h"
-void _puts(char *s)
+
+/**
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
 {
-	_putchar('0');
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
